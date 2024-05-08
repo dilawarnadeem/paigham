13feaa03590c33081a5b6d53125127067a909d9e
syed-zunair-gillani <zunairgillani54@gmail.com>
2024-05-08 12:31:15 +0500

    update

--- src/components/video-section/card.tsx
+++ src/components/video-section/card.tsx
@@ -35,7 +35,7 @@ const Card = ({ item, OpenVideo, slug, textColor }: any) => {
             alt="image"
             width={700}
             height={400}
-            className=" w-full object-cover transition-all h-[220px] duration-200 ease-in-out"
+            className=" w-full object-cover transition-all h-[150px] md:h-[170px] duration-200 ease-in-out"
           />
           <div className=" group-hover:bg-black/40 absolute inset-0 group-hover:cursor-pointer p-3 md:p-6 flex flex-col justify-end font-metapro "/>
         </div>
