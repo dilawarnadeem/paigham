4f388f94e16d2200a693547a269027d162126587
mufaqar <mufaqar@gmail.com>
2024-06-13 15:42:13 +0500

    Channels

--- src/pages/channel/tv-urdu.jsx
+++ src/pages/channel/tv-urdu.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 const TVUrdu = () => {
   return (
-
-    <div className='h-screen text-center flex flex-col items-center justify-center'>
-      <iframe 
-        width="100%" 
-        height="100%" 
-        src="https://www.youtube.com/embed/6PqM5vszwmk?si=8L7kkYzZ22wykN58" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
+    <div className="h-screen text-center flex flex-col items-center justify-center">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/6PqM5vszwmk?si=8L7kkYzZ22wykN58"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
       ></iframe>
-      <a href="https://sscsott.com/pk/paighamtv/index.m3u8" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+      <a
+        href="https://sscsott.com/pk/paighamtv/index.m3u8"
+        className="px-10 py-2 bg-blue text-white rounded  block my-10 "
+      >
         Live URDU
       </a>
     </div>
   );
-}
+};
 
 export default TVUrdu;
