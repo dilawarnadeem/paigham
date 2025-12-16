4cbf7d91b2cef272c44c80365d1d528127648eb5
Mufaqar <mufaqar@gmail.com>
2025-12-16 11:53:44 +0500

    updated

--- src/components/header/topBar.tsx
+++ src/components/header/topBar.tsx
@@ -24,13 +24,13 @@ const TopBar = () => {
     <>
       {/* top bar with smooth scrolling  */}
       <div className="overflow-x-hidden bg-secondary">
-        <div className="py-2">
+        <div className="py-1">
         <Marquee direction="right">
           {hadith?.newsTickers?.newsTicker?.map((item: any, idx: any) => {
             return (
               <div className="flex items-center" key={idx}>
                 <div className="p-[2px] px-3 bg-blue" />
-                <span className="mx-2 text-sm md:text-xl font-mehr text-[#050007] ">
+                <span className="mx-2 text-sm md:text-lg leading-8 font-mehr text-[#050007] ">
                      {item.info}
                 </span>
               </div>
