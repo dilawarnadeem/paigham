c83433114cd1119025a05336e1004a7ec6c115cc
dilawarnadeem <157968649+dilawarnadeem@users.noreply.github.com>
2026-01-07 22:33:11 +0500

    Update index.tsx

--- src/pages/paigham-membership-list/index.tsx
+++ src/pages/paigham-membership-list/index.tsx
@@ -1,1 +1,35 @@
+"use client";
+
+import React from "react";
+import PageBanner from "@/components/pageBanner/PageBanner";
+import SeoMeta from "@/components/seo";
+
+export default function PaighamClubMembers() {
+  return (
+    <>
+      {/* SEO */}
+      <SeoMeta
+        title="Paigham Club Members | Paigham TV"
+        url="/club-membership-list"
+        description="Paigham Club Members Verification Page"
+      />
+
+      {/* Page Banner */}
+      <PageBanner
+        title="Paigham Club Members"
+        image="/images/banner-2.jpg"
+      />
+
+      {/* Iframe Section */}
+      <section className="w-full h-screen">
+        <iframe
+          src="https://zamzamwelfaretrust.com/paighamtv/club-membership-list/"
+          title="Paigham Club Members"
+          className="w-full h-full border-none"
+          loading="lazy"
+        />
+      </section>
+    </>
+  );
+}
 
